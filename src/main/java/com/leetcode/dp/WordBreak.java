b91a7a885de0ccd46f7a6fab52f96c3c30c9e309
Embrace <729374717@qq.com>
2022-05-18 11:33:02 +0800

    单词拆分

---
+++ src/main/java/com/leetcode/dp/WordBreak.java
@@ -0,0 +1,69 @@
+package com.leetcode.dp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Embrace
+ * git: https://gitee.com/EmbraceQAQ
+ * @version 1.0
+ * @since JDK 1.8
+ * Date: 2022/5/18 11:18
+ * Description: 单词拆分
+ * FileName: WordBreak
+ */
+public class WordBreak {
+    /**
+     * 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+     * <p>
+     * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入: s = "leetcode", wordDict = ["leet", "code"]
+     * 输出: true
+     * 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+     * 示例 2：
+     * <p>
+     * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+     * 输出: true
+     * 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+     *      注意，你可以重复使用字典中的单词。
+     * 示例 3：
+     * <p>
+     * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+     * 输出: false
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/word-break
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                String substring = s.substring(j, i);
+                if (dp[j] && wordDictSet.contains(substring)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        List<String> d=new ArrayList<>();
+        d.add("leet");
+        d.add("code");
+        System.out.println(wordBreak("leetcode",d));
+    }
+
+}
