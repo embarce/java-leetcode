fd7d11dd298408609e5beff5acf316a04d0f64f0
Embrace <729374717@qq.com>
2022-06-02 10:19:13 +0800

    删除二叉搜索树中的节点

---
+++ src/main/java/com/leetcode/tree/DeleteNode.java
@@ -0,0 +1,52 @@
+package com.leetcode.tree;
+
+import com.leetcode.vo.TreeNode;
+import org.apache.poi.ss.formula.functions.T;
+
+/**
+ * @author Embrace
+ * git: https://github.com/embarce
+ * @version 1.0
+ * @since JDK 1.8
+ * Date: 2022/6/2 10:17
+ * Description: 删除二叉搜索树中的节点
+ * FileName: DeleteNode
+ */
+public class DeleteNode {
+    /**
+     * 450. 删除二叉搜索树中的节点
+     * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+     *
+     * 一般来说，删除节点可分为两个步骤：
+     *
+     * 首先找到需要删除的节点；
+     * 如果找到了，删除它。
+     *
+     *
+     * 示例 1:
+     *
+     *
+     *
+     * 输入：root = [5,3,6,2,4,null,7], key = 3
+     * 输出：[5,4,6,2,null,null,7]
+     * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+     * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+     * 另一个正确答案是 [5,2,6,null,4,null,7]。
+     *
+     *
+     * 示例 2:
+     *
+     * 输入: root = [5,3,6,2,4,null,7], key = 0
+     * 输出: [5,3,6,2,4,null,7]
+     * 解释: 二叉树不包含值为 0 的节点
+     * 示例 3:
+     *
+     * 输入: root = [], key = 0
+     * 输出: []
+     */
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        // TODO: 2022/6/2 今天先放着，有重要事情，明天再来跟新
+        return new TreeNode();
+    }
+}
