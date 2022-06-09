e669dfb619dc362ee4a4c9803938194139d135e9
Embrace <729374717@qq.com>
2022-06-09 11:29:45 +0800

    前 K 个高频元素

---
+++ src/main/java/com/leetcode/arrayTest/TopKFrequent.java
@@ -0,0 +1,67 @@
+package com.leetcode.arrayTest;
+
+import java.util.*;
+
+/**
+ * @author Embrace
+ * git: https://github.com/embarce
+ * @version 1.0
+ * @since JDK 1.8
+ * Date: 2022/6/9 11:14
+ * Description: 前 K 个高频元素
+ * FileName: TopKFrequent
+ */
+public class TopKFrequent {
+
+    /**
+     * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+     *
+     *  
+     *
+     * 示例 1:
+     *
+     * 输入: nums = [1,1,1,2,2,3], k = 2
+     * 输出: [1,2]
+     * 示例 2:
+     *
+     * 输入: nums = [1], k = 1
+     * 输出: [1]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/top-k-frequent-elements
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] m, int[] n) {
+                return m[1] - n[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+}
