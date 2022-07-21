c1ffa53153de968326f8da49ddaa2b773a997b34
Embrace <729374717@qq.com>
2022-07-21 20:56:11 +0800

    814. 二叉树剪枝

---
+++ src/main/java/com/leetcode/tree/PruneTree.java
@@ -0,0 +1,28 @@
+package com.leetcode.tree;
+
+import com.leetcode.vo.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author Embrace
+ * git: https://github.com/embarce
+ * @version 1.0
+ * @since JDK 1.8
+ * Date: 2022/7/21 20:43
+ * Description:
+ * FileName: PruneTree
+ */
+public class PruneTree {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+}
